84ae651602d20a928f25affcbb254dca78c058bb
Eneko Bikandi <eneko.bikandi@ve.com>
2020-04-03 14:59:45 +0200

    [Site]: global and bundle scoped React versions

---
+++ some-site/src/components/button.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const onClick = () => {
+  const { version } = require("react")
+  console.log("[dbg]: Site bundled React version", version)
+  console.log("[dbg]: window's React version", window.React.version)
+
+  const event = new CustomEvent("trigger-app")
+  window.dispatchEvent(event)
+}
+export default () => (
+  <button type="button" onClick={onClick}>
+    Click me!
+  </button>
+)
--- some-site/src/pages/page-2.js
+++ some-site/src/pages/page-2.js
@@ -3,12 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Button from "../components/button"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Button />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

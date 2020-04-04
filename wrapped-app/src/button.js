0e660fcb0a90aee48d417d73e275a4924e33b51b
Eneko Bikandi <eneko.bikandi@ve.com>
2020-04-04 17:27:26 +0200

    [App]: log versions in the DOM

---
+++ wrapped-app/src/button.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const COLORS = ['yellow', 'antiquewhite'];
+export default () => {
+  const [versions, setVersions] = useState({});
+  const [color, setColor] = useState(COLORS[0]);
+  const handleClick = () => {
+    const { version } = require('react');
+    setVersions({});
+    setVersions({ bundle: version, global: window.React?.version });
+    setColor(current => COLORS.find(c => c !== current));
+  };
+  return (
+    <>
+      <button id="root" type="button" onClick={handleClick}>
+        Get versions
+      </button>
+      {versions.bundle && (
+        <div id="nko-log" style={{ backgroundColor: color, color: 'black' }}>
+          <h3>App: {versions.bundle}</h3>
+          <h3>Window: {versions.global}</h3>
+        </div>
+      )}
+    </>
+  );
+};

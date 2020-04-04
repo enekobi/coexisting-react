acbd25f11da190dcf3b34cb22b37464a061de8f3
Eneko Bikandi <eneko.bikandi@ve.com>
2020-04-04 17:09:16 +0200

    [Site]: log React version in the DOM

--- some-site/src/components/button.js
+++ some-site/src/components/button.js
@@ -1,15 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 
-const onClick = () => {
-  const { version } = require("react")
-  console.log("[dbg]: Site bundled React version", version)
-  console.log("[dbg]: window's React version", window.React.version)
-
-  const event = new CustomEvent("trigger-app")
-  window.dispatchEvent(event)
+const COLORS = ["aquamarine", "sandybrown"]
+export default () => {
+  const [versions, setVersions] = useState({})
+  const [color, setColor] = useState(COLORS[0])
+  const handleClick = () => {
+    const { version } = require("react")
+    setVersions({})
+    setVersions({ bundle: version, global: window.React.version })
+    setColor(current => COLORS.find(c => c !== current))
+    const event = new CustomEvent("trigger-app")
+    window.dispatchEvent(event)
+  }
+  return (
+    <>
+      <h1>Click button to refresh React version logs:</h1>
+      <button id="root" type="button" onClick={handleClick}>
+        Click me!
+      </button>
+      {versions.bundle && (
+        <div id="nko-log" style={{ backgroundColor: color }}>
+          <h3>Site: {versions.bundle}</h3>
+          <h3>Global: {versions.global}</h3>
+        </div>
+      )}
+    </>
+  )
 }
-export default () => (
-  <button id="root" type="button" onClick={onClick}>
-    Click me!
-  </button>
-)
--- some-site/src/utils/injectScripts.js
+++ some-site/src/utils/injectScripts.js
@@ -24,24 +24,24 @@ let oldReactInjected = false
   }
 })()
 
-!(function() {
-  if (!appInjected) {
-    console.log("[dbg]: injecting app ")
-    appInjected = true
-    var a = document.createElement("script")
-    a.id = "ve-tag"
-    a.type = "text/javascript"
-    a.async = !0
-    a.src = "//localhost:3000/static/js/bundle.js"
-    var b = document.getElementsByTagName("head")[0]
-    if (b) {
-      // console.log("[dbg]: appending to <head>")
-      b.appendChild(a, b)
-    } else {
-      // console.log("[dbg]: getting other <scripts>")
-      var b = document.getElementsByTagName("script")[0]
-      // console.log("[dbg]: other <scripts>", b)
-      b.parentNode.insertBefore(a, b)
-    }
-  }
-})()
+// !(function() {
+//   if (!appInjected) {
+//     console.log("[dbg]: injecting app ")
+//     appInjected = true
+//     var a = document.createElement("script")
+//     a.id = "ve-tag"
+//     a.type = "text/javascript"
+//     a.async = !0
+//     a.src = "//localhost:3000/static/js/bundle.js"
+//     var b = document.getElementsByTagName("head")[0]
+//     if (b) {
+//       // console.log("[dbg]: appending to <head>")
+//       b.appendChild(a, b)
+//     } else {
+//       // console.log("[dbg]: getting other <scripts>")
+//       var b = document.getElementsByTagName("script")[0]
+//       // console.log("[dbg]: other <scripts>", b)
+//       b.parentNode.insertBefore(a, b)
+//     }
+//   }
+// })()
